9142b9e6c7f26d1ca3f9757c05811fafa47143a7
th3n1ghtmanc0m3th <jaywehrman@gmail.com>
2020-12-10 18:51:08 -0800

    Piezas Game implementation and tests

--- Piezas.cpp
+++ Piezas.cpp
@@ -13,23 +13,35 @@
  * [0,0][0,1][0,2][0,3]
  * So that a piece dropped in column 2 should take [0,2] and the next one
  * dropped in column 2 should take [1,2].
-**/
+ **/
 
 
 /**
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
-**/
+ **/
 Piezas::Piezas()
 {
+	/* build board */
+	turn = X;
+	for(int row = 0; row < BOARD_ROWS; row++) {
+		for(int col = 0; col < BOARD_COLS; col++) {
+			board[row][col] = Blank;
+		}
+	}
 }
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
-**/
+ **/
 void Piezas::reset()
 {
+	for(int row = 0; row < BOARD_ROWS; row++) {
+		for(int col = 0; col < BOARD_COLS; col++) {
+			board[row][col] = Blank;
+		}
+	}
 }
 
 /**
@@ -39,19 +51,57 @@ void Piezas::reset()
  * In that case, placePiece returns Piece Blank value 
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+ **/ 
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+	/* store piece to drop */
+	Piece droppedPiece = turn;
+
+	/* check and assign turn */
+	if(turn == X) {
+		turn = O;
+	}
+	else {
+		turn = X;
+	}
+
+	/* check bounds */
+	if(column < 0 || column > BOARD_COLS) {
+		return Invalid;
+	}
+
+	/* check column availability */
+	for(int row = 0; row < BOARD_ROWS; row++) {
+		if(pieceAt(row, column) == Blank) {
+			return droppedPiece;
+		}
+	}
+
+	/* Something real bad happened or the column is filled */
+	return Blank;
+
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+	/* check bounds */
+	if(row > BOARD_ROWS || column > BOARD_COLS) {
+		return Invalid;
+	}
+	/* check occupancy */
+	if(board[row][column] == X) {
+		return X;
+	}
+	else if(board[row][column] == O) {
+		return O;
+	}
+
+	/* no piece placed yet */
+	return Blank;
 }
 
 /**
@@ -62,8 +112,147 @@ Piece Piezas::pieceAt(int row, int column)
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
-**/
+ **/
 Piece Piezas::gameState()
 {
-    return Blank;
+	/* is board filled? */
+	bool blankFlag = false;
+	for(int i = 0; i < BOARD_COLS; i++) {
+		blankFlag = board[i].size() != BOARD_ROWS;
+	}
+
+	/* a blank indicates game still in play */
+	if(blankFlag) {
+		return Invalid;
+	}
+
+	/* winner counts vars */
+	int numX = 0;
+	int numO = 0;
+
+	/* CHECK COLUMNS */
+	/* check for X winner in columns */
+	for(int col = 0; col < BOARD_COLS; col++) {
+		int tempMax = 0;
+		int tempCount = 0;
+		Piece lastPlayed = X;
+
+		/* check for x's in column */
+		for(int row = 0; row < BOARD_ROWS; row++) {
+			/* check for streak */
+			if(board[col][row] == X && board[col][row] == lastPlayed) {
+				tempCount++;
+				/* if current streak longer than max, set it */
+				if(tempCount > tempMax) {
+					tempMax = tempCount;
+				}
+			}
+			/* steak was broken */
+			else {
+				tempCount = 0;
+			}
+			/* set last played piece */
+			lastPlayed = board[col][row];
+		}
+		/* set max X */
+		if(tempMax > numX) {
+			numX = tempMax;
+		}
+	}
+	/* check for O winner in columns */
+	for(int col = 0; col < BOARD_COLS; col++) {
+		int tempMax = 0;
+		int tempCount = 0;
+		Piece lastPlayed = O;
+
+		/* check for O's in column */
+		for(int row = 0; row < BOARD_ROWS; row++) {
+			/* check for streak */
+			if(board[col][row] == O && board[col][row] == lastPlayed) {
+				tempCount++;
+				/* if current streak longer than max, set it */
+				if(tempCount > tempMax) {
+					tempMax = tempCount;
+				}
+			}
+			/* streak was broken */
+			else {
+				tempCount = 0;
+			}
+			/* set last played piece */
+			lastPlayed = board[col][row];
+		}
+		/* set max O */
+		if(tempMax > numO) {
+			numO = tempMax;
+		}
+	}
+
+	/* CHECK ROWS */
+	/* check for X winner in rows */
+	for(int row = 0; row < BOARD_ROWS; row++) {
+		int tempMax = 0;
+		int tempCount = 0;
+		Piece lastPlayed = X;
+
+		/* check for x's in column */
+		for(int col = 0; col < BOARD_COLS; col++) {
+			/* check for streak */
+			if(board[col][row] == X && board[col][row] == lastPlayed) {
+				tempCount++;
+				/* if current streak longer than max, set it */
+				if(tempCount > tempMax) {
+					tempMax = tempCount;
+				}
+			}
+			/* steak was broken */
+			else {
+				tempCount = 0;
+			}
+			/* set last played piece */
+			lastPlayed = board[col][row];
+		}
+		/* set max X */
+		if(tempMax > numX) {
+			numX = tempMax;
+		}
+	}
+	/* check for O winner in rows */
+	for(int row = 0; row < BOARD_ROWS; row++) {
+		int tempMax = 0;
+		int tempCount = 0;
+		Piece lastPlayed = O;
+
+		/* check for O's in column */
+		for(int col = 0; col < BOARD_COLS; col++) {
+			/* check for streak */
+			if(board[col][row] == O && board[col][row] == lastPlayed) {
+				tempCount++;
+				/* if current streak longer than max, set it */
+				if(tempCount > tempMax) {
+					tempMax = tempCount;
+				}
+			}
+			/* steak was broken */
+			else {
+				tempCount = 0;
+			}
+			/* set last played piece */
+			lastPlayed = board[col][row];
+		}
+		/* set max O */
+		if(tempMax > numO) {
+			numO = tempMax;
+		}
+	}
+
+	/* who won */
+	if(numX > numO) {
+		return X;
+	}
+	if(numO > numX) {
+		return O;
+	}
+	/* tie */
+	return Blank;
 }
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -1,10 +1,10 @@
 /**
  * Unit Tests for Piezas
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +17,160 @@ class PiezasTest : public ::testing::Test
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, dropPieceValid)
+{
+	Piezas board;
+	for (int i=0; i < BOARD_COLS; i++) {
+		Piece droppedPiece = board.dropPiece(i);
+		ASSERT_TRUE(droppedPiece != Invalid && droppedPiece != Blank);
+	}
+}
+
+TEST(PiezasTest, dropPieceInvalidNegNum)
+{
+	Piezas board;
+	ASSERT_TRUE(board.dropPiece(-3) == Invalid);
+}
+
+TEST(PiezasTest, dropPieceInvalidNumBig)
+{
+	Piezas board;
+	ASSERT_TRUE(board.dropPiece(9) == Invalid);
+}
+
+TEST(PiezasTest, dropPieceBoardFull)
+{
+	Piezas board;
+	for (int i=0; i < BOARD_ROWS; i++) {
+		board.dropPiece(0);
+	}
+	ASSERT_TRUE(board.dropPiece(0) == Blank);
+}
+
+TEST(PiezasTest, pieceAtEmptyValid)
+{
+	Piezas board;
+	for (int col=0; col < BOARD_COLS; col++) {
+		for (int row=0; row < BOARD_ROWS; row++) {
+			ASSERT_TRUE(board.pieceAt(row, col) == Blank);
+		}
+	}
+}
+
+TEST(PiezasTest, pieceAtFullValid)
+{
+	Piezas board;
+	for (int col=0; col < BOARD_COLS; col++) {
+		for (int row=0; row < BOARD_ROWS; row++) {
+			board.dropPiece(col);
+		}
+	}
+
+	for (int col=0; col < BOARD_COLS; col++) {
+		for (int row=0; row < BOARD_ROWS; row++) {
+			Piece droppedPiece = board.pieceAt(row, col);
+			ASSERT_TRUE(droppedPiece != Invalid && droppedPiece != Blank);
+		}
+	}
+}
+
+TEST(PiezasTest, gameStateEmpty)
+{
+	Piezas board;
+	ASSERT_TRUE(board.gameState() == Invalid);
+}
+
+TEST(PiezasTest, gameStateTie)
+{
+	Piezas board;
+	for (int col=0; col < BOARD_COLS; col++) {
+		for (int row=0; row < BOARD_ROWS; row++) {
+			board.dropPiece(col);
+		}
+	}
+	ASSERT_TRUE(board.gameState() == Blank);
+}
+
+TEST(PiezasTest, XwinCol)
+{
+	Piezas board;
+	for (int col=1; col < BOARD_COLS; col++) {
+		for (int row=0; row < BOARD_ROWS; row++) {
+			board.dropPiece(col);
+		}
+	}
+	for (int row=0; row < BOARD_ROWS; row++) {
+		/* give O invalid piece */
+		board.dropPiece(-1);
+		/* give X valid piece */
+		board.dropPiece(0);
+	}
+	ASSERT_TRUE(board.gameState() == X);
+}
+
+TEST(PiezasTest, OwinCol)
+{
+	Piezas board;
+	for (int col=1; col < BOARD_COLS; col++) {
+		for (int row=0; row < BOARD_ROWS; row++) {
+			board.dropPiece(col);
+		}
+	}
+	for (int row=0; row < BOARD_ROWS; row++) {
+		/* give O valid piece */
+		board.dropPiece(0);
+		/* give X bad piece */
+		board.dropPiece(-1);
+	}
+	ASSERT_TRUE(board.gameState() == O);
+}
+
+TEST(PiezasTest, XwinRow)
+{
+	Piezas board;
+	for (int col=0; col < BOARD_COLS; col++) {
+		board.dropPiece(col);
+		board.dropPiece(-1);
+	}
+
+	board.dropPiece(0);
+	board.dropPiece(0);
+	board.dropPiece(2);
+	board.dropPiece(2);
+
+	board.dropPiece(-1);
+
+	board.dropPiece(1);
+	board.dropPiece(1);
+	board.dropPiece(3);
+	board.dropPiece(3);
+
+	ASSERT_TRUE(board.gameState() == X);
+}
+
+TEST(PiezasTest, OwinRow)
+{
+	Piezas board;
+
+	board.dropPiece(-1);
+
+	for (int col=0; col < BOARD_COLS; col++) {
+		board.dropPiece(col);
+		board.dropPiece(-1);
+	}
+	board.dropPiece(0);
+	board.dropPiece(0);
+	board.dropPiece(2);
+	board.dropPiece(2);
+
+	board.dropPiece(-1);
+
+	board.dropPiece(1);
+	board.dropPiece(1);
+	board.dropPiece(3);
+	board.dropPiece(3);
+
+	ASSERT_TRUE(board.gameState() == O);
 }
